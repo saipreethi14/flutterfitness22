9765e9d1ea6da43a1b37294a7c8c026180c95d02
JatinderK325 <74372431+JatinderK325@users.noreply.github.com>
2021-12-17 09:08:03 -0500

    bmi class edited

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -3,20 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'plusMinus.dart';
 import 'dart:math';
+import 'gender.dart';
+
+enum Gender {
+  male,
+  female,
+}
+Gender selectedGender = Gender.male;
+
+const activeColor = Color(0xFFFFFFFF);
+const inactiveColor = Colors.grey;
+
+const maleColor = inactiveColor;
+const femaleColor = inactiveColor;
+
 
 int age = 5;
 double height = 5;
 double weight = 40;
 double result = 20;
 
-class bmi extends StatefulWidget {
-  const bmi({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  _bmiState createState() => _bmiState();
+  _HomeState createState() => _HomeState();
 }
 
-class _bmiState extends State<bmi> {
+class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
@@ -68,6 +82,58 @@ class _bmiState extends State<bmi> {
                   ),
                 ),
               ),
+              //.............................................
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  children: <Widget>[
+                    Gender1(
+                      otp: (){
+                        setState(() {
+                          selectedGender = Gender.male;
+                        });
+                      },
+                      contColor: selectedGender == Gender.male ? activeColor : inactiveColor,
+                      customChild: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.mars,
+                                size: 70, color: Colors.black),
+                            SizedBox(height: 10.0),
+                            Text('Male', style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),),
+                          ]
+                      ),
+                    ),
+
+                    Gender1(
+                      otp: (){
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                      contColor: selectedGender == Gender.female ? activeColor : inactiveColor,
+                      customChild: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.venus,
+                              size: 70, color: Colors.black,),
+                            SizedBox(height: 10.0),
+                            Text('Female', style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),),
+                          ]
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //................................
               Padding(
                 padding: const EdgeInsets.only(top: 18.0, left:28.0, right: 28.0),
                 child: TextField(
@@ -190,7 +256,7 @@ class _bmiState extends State<bmi> {
                       ],
                     ),
                     style: TextButton.styleFrom(
-                      fixedSize: Size.fromWidth(140),
+                      fixedSize: Size.fromWidth(400),
                       primary: Colors.black,
                       onSurface: Colors.red,
                       backgroundColor: Colors.white,
@@ -205,3 +271,4 @@ class _bmiState extends State<bmi> {
     );
   }
 }
+
